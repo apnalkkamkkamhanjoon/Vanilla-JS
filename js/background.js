2d629a06b380b9fe2c27f5a1bcdfc4cc36be968d
cutejoon <s22073@gsm.hs.kr>
2022-09-01 20:58:17 +0900

    0901

---
+++ js/background.js
@@ -0,0 +1,9 @@
+const images = ["1.jpg", "2.jpg", "3.jpg"];
+
+const chooseImages = images[Math.floor(Math.random() * images.length)];
+
+const backgroundImage = document.createElement("img");
+
+backgroundImage.src = `img/${chooseImages}`;
+
+document.body.appendChild(backgroundImage);
